687af20ad1dafa54b0ab296d163930a37c69b2da
AM0312 <ansh.midha0312@gmail.com>
2023-01-30 21:11:09 +0530

    Salting and Hashing

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express=require('express');
 const bodyParser=require("body-parser");
 const ejs=require('ejs');
 const mongoose=require("mongoose");
-const md5=require('md5');
+const bcrypt=require('bcrypt');
+const saltRouns=10;
+
 const app=express();
 
 app.use(express.static("public"));
@@ -33,25 +35,29 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-    const newUser=new User({
-        email:req.body.username,
-        password:md5(req.body.password)
-    });
-    newUser.save(function(err){
-        if(err) console.log(err);
-        else res.render("secrets");
-    });
+    bcrypt.hash(req.body.password,saltRouns,function(err,hash){
+        const newUser=new User({
+            email:req.body.username,
+            password:hash
+        });
+        newUser.save(function(err){
+            if(err) console.log(err);
+            else res.render("secrets");
+        });
+    });    
 });
 
 app.post("/login",function(req,res){
     const userName=req.body.username;
-    const password=md5(req.body.password);
+    const password=req.body.password;
 
     User.findOne({email:userName},function(err,foundUser){
         if(err) console.log((err));
         else{
             if(foundUser){
-                if(foundUser.password===password) res.render("secrets");
+                bcrypt.compare(password,foundUser.password,function(err,result){
+                    if(result) res.render("/secrets");
+                });
             }
         }
     });
